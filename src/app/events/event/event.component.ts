4fa13b9b4e1ba5ce799a0c951811b67cd6a29862
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-08-08 22:59:09 +0900

    [update]

--- src/app/events/event/event.component.ts
+++ src/app/events/event/event.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router'
+import { GlobalDataService } from '../../services/global-data.service';
 
 @Component({
-  selector: 'app-event',
-  templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+    selector: 'app-event',
+    templateUrl: './event.component.html',
+    styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
+    public loading = true;
+    public id?: string;
 
-  constructor() { }
+    constructor(
+        public global: GlobalDataService,
+        private router: Router,
+        private route: ActivatedRoute
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.id = this.route.snapshot.paramMap.get('id') || undefined;
+
+        this.global.auth.authState.subscribe((user) => {
+            if (user == null) {
+                this.router.navigate(["/accounts/login"]);
+                return;
+            }
+            this.global.initialize().then(() => {
+                this.loading = false;
+            });
+        });
+    }
 
 }
