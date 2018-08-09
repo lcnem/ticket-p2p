045ad8bfa3f806c541779b7b10dcf097701b8be4
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-08-09 19:55:38 +0900

    [update]input dialog

--- src/app/components/input-dialog/input-dialog.component.ts
+++ src/app/components/input-dialog/input-dialog.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
-  selector: 'app-input-dialog',
-  templateUrl: './input-dialog.component.html',
-  styleUrls: ['./input-dialog.component.css']
+    selector: 'app-input-dialog',
+    templateUrl: './input-dialog.component.html',
+    styleUrls: ['./input-dialog.component.css']
 })
 export class InputDialogComponent implements OnInit {
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: {
+            title: string,
+            placeholder: string,
+            inputData: string,
+            inputType?: string,
+            cancel: string,
+            submit: string,
+        }
+    ) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/events/event/event.component.ts
+++ src/app/events/event/event.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router'
 import { GlobalDataService } from '../../services/global-data.service';
+import { MatDialog } from '@angular/material';
+import { DialogComponent } from '../../components/dialog/dialog.component';
+import { LoadingDialogComponent } from '../../components/loading-dialog/loading-dialog.component';
+import { HttpClient } from '@angular/common/http';
+
+declare let Payjp: any;
 
 @Component({
     selector: 'app-event',
@@ -10,11 +16,15 @@ import { GlobalDataService } from '../../services/global-data.service';
 export class EventComponent implements OnInit {
     public loading = true;
     public id?: string;
+    public eventName?: string;
+    public capacity?: number;
 
     constructor(
         public global: GlobalDataService,
         private router: Router,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private dialog: MatDialog,
+        private http: HttpClient
     ) {
     }
 
@@ -32,4 +42,147 @@ export class EventComponent implements OnInit {
         });
     }
 
+    public async onBuyClicked() {
+        if (!(window as any).PaymentRequest) {
+            this.dialog.open(DialogComponent, {
+                data: {
+                    title: this.translation.error[this.global.lang],
+                    content: ""
+                }
+            });
+            return;
+        }
+        let supportedInstruments: PaymentMethodData[] = [{
+            supportedMethods: ['basic-card'],
+            data: {
+                supportedNetworks: [
+                    'visa',
+                    'mastercard',
+                    'amex',
+                    'diners',
+                    'jcb'
+                ]
+            }
+        }];
+
+        let details = {
+            displayItems: [
+                {
+                    label: this.translation.fee[this.global.lang],
+                    amount: {
+                        currency: "JPY",
+                        value: "0"
+                    }
+                }
+            ],
+            total: {
+                label: this.translation.total[this.global.lang],
+                amount: {
+                    currency: "JPY",
+                    value: "0"
+                }
+            }
+        };
+
+        let request = new PaymentRequest(supportedInstruments, details, { requestShipping: false });
+
+        let result = await request.show();
+        if(!result) {
+            return;
+        }
+
+        let dialogRef = this.dialog.open(LoadingDialogComponent, { disableClose: true });
+
+        Payjp.setPublicKey("pk_test_0383a1b8f91e8a6e3ea0e2a9");
+        Payjp.createToken({
+            number: result.details.cardNumber,
+            cvc: result.details.cardSecurityCode,
+            exp_month: result.details.expiryMonth,
+            exp_year: result.details.expiryYear
+        }, async (status: any, response: any) => {
+            try {
+                if (status == 200) {
+                    await this.http.post(
+                        "",
+                        {
+                            token: response.id
+                        }
+                    ).toPromise();
+                } else {
+                    throw Error();
+                }
+            } catch {
+                this.dialog.open(DialogComponent, {
+                    data: {
+                        title: this.translation.error[this.global.lang],
+                        content: ""
+                    }
+                });
+                result.complete("fail");
+
+                return;
+            } finally {
+                result.complete("success");
+                dialogRef.close();
+            }
+    
+            this.dialog.open(DialogComponent, {
+                data: {
+                    title: this.translation.completed[this.global.lang],
+                    content: ""
+                }
+            }).afterClosed().subscribe(() => {
+                this.router.navigate([""]);
+            });
+            
+        });
+    }
+
+    
+    public translation = {
+        amount: {
+            en: "Amount",
+            ja: "金額"
+        },
+        error: {
+            en: "Error",
+            ja: "エラー"
+        },
+        completed: {
+            en: "Completed",
+            ja: "完了"
+        },
+        unsupported: {
+            en: "Request Payment API is not supported in this browser.",
+            ja: "Request Payment APIがこのブラウザではサポートされていません。"
+        },
+        fee: {
+            en: "Fee",
+            ja: "手数料"
+        },
+        total: {
+            en: "Total",
+            ja: "合計"
+        },
+        eventId: {
+            en: "Event ID",
+            ja: "イベントID"
+        },
+        eventName: {
+            en: "Event name",
+            ja: "イベント名"
+        },
+        edit: {
+            en: "Edit",
+            ja: "編集"
+        },
+        capacity: {
+            en: "Capacity",
+            ja: "定員"
+        },
+        supplement: {
+            en: "Supplement",
+            ja: "枠追加"
+        }
+    } as { [key: string]: { [key: string]: string } };
 }
